0723a3e4129929eb9e637f882b556b7f43340d9b
Shah <shaht2@diebold.com>
2016-12-29 12:33:04 +0530

    Test change

---
+++ Custom_Task/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+
+namespace SimpleTask3
+{
+    public class SimpleTask3 : Task
+    {
+        private string myProperty;
+
+        // The [Required] attribute indicates a required property.
+        // If a project file invokes this task without passing a value
+        // to this property, the build will fail immediately.
+        [Required]
+        public string MyProperty
+        {
+            get
+            {
+                return myProperty;
+            }
+            set
+            {
+                myProperty = value;
+            }
+        }
+
+        public override bool Execute()
+        {
+            // Log a high-importance commment
+            Log.LogMessage(MessageImportance.High,
+                "The task was passed \"" + myProperty + "\".");
+            File.Copy("E:\\Rollover.log", "E:\\2\\Rollover.log",true);
+            return true;
+        }
+    }
+
+}
